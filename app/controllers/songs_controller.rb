16aa1ff00891bd0bb8bdb04b5445c59475c28540
Sean Kimzey <sean.kimzey@gmail.com>
2018-03-13 17:11:42 -0500

    added index and show routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,17 @@
 class SongsController < ApplicationController
+
+  def index
+    songs = Song.all
+    render json: songs.as_json 
+  end
+
+  def show
+    # the_id = params[:id]
+    song = Song.find_by(id: params[:id])
+    render json: song.as_json
+  end
+
+  
 end
+
+
